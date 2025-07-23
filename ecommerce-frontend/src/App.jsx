2dcb0fc369f77e6408df33f74a5001bdc64a5ecd
srithars <64460687+srithars@users.noreply.github.com>
2025-07-23 15:36:12 +0530

    Update App.jsx

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/frontapp1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
