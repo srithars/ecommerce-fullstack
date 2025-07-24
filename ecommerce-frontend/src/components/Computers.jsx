e0abf19878dc8b0bbc40209696119d2aab2d947a
srithars <64460687+srithars@users.noreply.github.com>
2025-07-24 21:42:34 +0530

    Update Computers.jsx

--- ecommerce-frontend/src/components/Computers.jsx
+++ ecommerce-frontend/src/components/Computers.jsx
@@ -4,7 +4,7 @@ import { getProducts } from "../services/productService";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://localhost:9090/back1';
+const BASE_URL = 'http://54.172.97.72:9090/springapp2';
 
 const Computers = () => {
   const [products, setProducts] = useState([]);
