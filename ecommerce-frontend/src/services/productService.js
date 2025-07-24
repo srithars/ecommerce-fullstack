0e6fae7784e903f1e790dd649ae604810945df3d
srithars <64460687+srithars@users.noreply.github.com>
2025-07-24 21:50:04 +0530

    Update productService.js

--- ecommerce-frontend/src/services/productService.js
+++ ecommerce-frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:9090/back1/api/products";  // Updated Backend API URL
+const API_BASE_URL = "http://54.172.97.72:9090/springapp2/api/products";  // Updated Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
