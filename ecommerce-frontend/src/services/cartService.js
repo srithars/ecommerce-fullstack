7a952db91cdfd8addd1030eabeb7ee9a2617e752
srithars <64460687+srithars@users.noreply.github.com>
2025-07-24 21:49:51 +0530

    Update cartService.js

--- ecommerce-frontend/src/services/cartService.js
+++ ecommerce-frontend/src/services/cartService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Use the correct base URL for cart operations
-const CART_API_URL = "http://localhost:9090/back1/api/cart";
+const CART_API_URL = "http://54.172.97.72:9090/springapp2/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${CART_API_URL}/add`, {
